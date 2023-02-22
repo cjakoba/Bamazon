23d39be0bc2336128e48162c01993aea2e0d326d
cjakoba <cajakob@icloud.com>
2023-02-22 12:04:42 -0500

    Added delete_item.

--- bamazon.h
+++ bamazon.h
@@ -16,6 +16,7 @@ typedef struct {
 	int onsale;
 } item;
 
+// Function declarations (function prototypes or function signatures)
 int read_db(char *filename);
 int write_db(char *filename);
 void show_items();
@@ -23,11 +24,12 @@ int sprint_item(char *s, item *c);
 item *find_item_num(int itemnum);
 int find_item_str(item **items, char *s);
 item *add_item(int itemnum, char *category, char *name, char size, int quantity, double cost, int onsale);
-item *update_item(int itemnum, category category, char *name, char size, double cost, int onsale);
+item *update_item(int itemnum, category category, char *name, char size, int quantity, double cost, int onsale);
 int get_category(item **items, category c);
 int get_category_size(item **items, category c, char size);
 int get_category_cost(item **items, category c, double cost);
 item *purchase_item(int itemnum);
+item *delete_item(int itemnum);
 int checkout(char **receipt);
 char *category_to_str(category c);
 category str_to_category(char *s);
